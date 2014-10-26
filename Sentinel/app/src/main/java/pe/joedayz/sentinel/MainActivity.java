74f6bea5ae4345a735f77d27bbc448b431ac091d
Jose Diaz <jose.diaz@joedayz.pe>
2014-10-25 20:08:56 -0500

    Asyntask uso

--- Sentinel/app/src/main/java/pe/joedayz/sentinel/MainActivity.java
+++ Sentinel/app/src/main/java/pe/joedayz/sentinel/MainActivity.java
@@ -1,28 +1,9 @@
 package pe.joedayz.sentinel;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -33,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -58,104 +39,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            String[] data = {
-                    "Lun 10/25 - Soleado - 31/17",
-                    "Mar 10/26 - Nublado - 21/8",
-                    "Mie 10/27 - Lluvioso - 18/11",
-                    "Jue 10/28 - Soleado - 31/17",
-                    "Vie 10/29 - Nublado - 21/8",
-                    "Sab 10/30 - Otro - 23/18",
-                    "Dom 10/31 - Soleado - 20/7"
-
-            };
-
-            List<String> weekForecast =
-                    new ArrayList<String>(Arrays.asList(data));
-
-            ArrayAdapter<String> forecastAdapter =
-                    new ArrayAdapter<String>(
-                            getActivity(),
-                            R.layout.list_item_forecast,
-                            R.id.list_item_forecast_textview,
-                            weekForecast
-
-                    );
-
-
-            View rootView = inflater.inflate(R.layout.fragment_main,
-                    container, false);
-
-
-            ListView listView = (ListView)
-                    rootView.findViewById(R.id.listview_forecast);
-
-
-            listView.setAdapter(forecastAdapter);
-
-
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            String forecastJsonStr = null;
-
-            try {
-
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    return null;
-                }
-
-                forecastJsonStr = buffer.toString();
-
-
-            } catch (IOException ex) {
-                Log.e("PlaceholderFragment", "Error", ex);
-                return null;
-            } finally {
-
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e("PlaceholderFragment", "Error closing stream", e);
-                    }
-                }
-            }
-
-
-            return rootView;
-        }
-    }
 }
