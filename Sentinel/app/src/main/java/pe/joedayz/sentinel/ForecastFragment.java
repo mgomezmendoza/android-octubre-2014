a5bd5cae2a32fb391c786760c4ceefd2a787ce5d
Jose Diaz <jose.diaz@joedayz.pe>
2014-10-25 20:40:08 -0500

    mostrando el refresh button en el fragment

--- Sentinel/app/src/main/java/pe/joedayz/sentinel/ForecastFragment.java
+++ Sentinel/app/src/main/java/pe/joedayz/sentinel/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -72,6 +75,29 @@ public class ForecastFragment
 
 
         return rootView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //esta linea es importante porque permite al fragmento
+        //manejar menu events
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public class FetchWeatherTask extends
