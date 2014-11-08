f25929a805ac13efda905a18d4f1a6a6fa648f56
Jose Diaz <jose.diaz@joedayz.pe>
2014-11-08 16:24:47 -0500

    built url

--- Sentinel/app/src/main/java/pe/joedayz/sentinel/ForecastFragment.java
+++ Sentinel/app/src/main/java/pe/joedayz/sentinel/ForecastFragment.java
@@ -1,5 +1,6 @@
 package pe.joedayz.sentinel;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -94,6 +95,9 @@ public class ForecastFragment
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.action_refresh){
+
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute("94043");
             return true;
         }
 
@@ -101,20 +105,47 @@ public class ForecastFragment
     }
 
     public class FetchWeatherTask extends
-            AsyncTask<Void, Void, Void>{
+            AsyncTask<String, Void, Void>{
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
+
+            if(params.length==0){
+                return null;
+            }
+
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
             String forecastJsonStr = null;
 
+            String format ="json";
+            String units = "metric";
+            int numDays = 7;
+
             try {
 
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                final String FORECAST_BASE_URL =
+                    "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+
+                Uri builUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .build();
+
+
+                URL url = new URL(builUri.toString());
+
+                Log.v(LOG_TAG, "Buildt URI " + builUri.toString());
+
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -138,6 +169,7 @@ public class ForecastFragment
 
                 forecastJsonStr = buffer.toString();
 
+                Log.v(LOG_TAG, "Forecast JSON String " + forecastJsonStr);
 
             } catch (IOException ex) {
                 Log.e(LOG_TAG, "Error", ex);
@@ -155,6 +187,8 @@ public class ForecastFragment
                     }
                 }
             }
+
+            Log.i(LOG_TAG, "salio...");
 
             return null;
         }
