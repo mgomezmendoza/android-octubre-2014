20139f9084f05a655e60a5ed5c1129279fe22a2b
Jose Diaz <jose.diaz@joedayz.pe>
2014-11-08 18:07:14 -0500

    display detail content

--- Sentinel/app/src/main/java/pe/joedayz/sentinel/DetailActivity.java
+++ Sentinel/app/src/main/java/pe/joedayz/sentinel/DetailActivity.java
@@ -1,5 +1,6 @@
 package pe.joedayz.sentinel;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,7 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-
+import android.widget.TextView;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -55,9 +56,20 @@ public class DetailActivity extends ActionBarActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            Intent intent = getActivity().getIntent();
+
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+            }
+
+
             return rootView;
         }
     }
