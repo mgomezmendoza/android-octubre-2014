3b44f9384d201a80ba328d101c5e8322bcc0f2c7
Jose Diaz Diaz <jose.diaz@joedayz.pe>
2015-01-24 17:40:08 -0500

    simple cursor adapter

--- Sentinel/app/src/main/java/pe/joedayz/sentinel/data/WeatherContract.java
+++ Sentinel/app/src/main/java/pe/joedayz/sentinel/data/WeatherContract.java
@@ -1,8 +1,11 @@
 package pe.joedayz.sentinel.data;
+
+
 import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,7 +18,7 @@ public class WeatherContract {
     // relationship between a domain name and its website.  A convenient string to use for the
     // content authority is the package name for the app, which is guaranteed to be unique on the
     // device.
-    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
+    public static final String CONTENT_AUTHORITY = "pe.joedayz.sentinel";
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
     // the content provider.
@@ -43,6 +46,21 @@ public class WeatherContract {
         // it must be converted to milliseconds in order to be converted to valid date.
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
         return sdf.format(date);
+    }
+
+    /**
+     * Converts a dateText to a long Unix time representation
+     * @param dateText the input date string
+     * @return the Date object
+     */
+    public static Date getDateFromDb(String dateText) {
+        SimpleDateFormat dbDateFormat = new SimpleDateFormat(DATE_FORMAT);
+        try {
+            return dbDateFormat.parse(dateText);
+        } catch ( ParseException e ) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     /* Inner class that defines the table contents of the location table */
